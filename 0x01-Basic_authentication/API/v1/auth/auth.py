b8b86a29db5abc75d521ea1f601c9aa3f5c70b0d
sanipop <67932273+sanipop@users.noreply.github.com>
2024-07-09 18:00:41 +0100

    Create auth.py

---
+++ 0x01-Basic_authentication/API/v1/auth/auth.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+"""script for Auth module.
+"""
+import re
+from typing import List, TypeVar
+from flask import request
+
+
+class Auth:
+    """Class for Auth.
+    """
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """path for auth.
+        """
+        if path is not None and excluded_paths is not None:
+            for exclusion_path in map(lambda x: x.strip(), excluded_paths):
+                pattern = ''
+                if exclusion_path[-1] == '*':
+                    pattern = '{}.*'.format(exclusion_path[0:-1])
+                elif exclusion_path[-1] == '/':
+                    pattern = '{}/*'.format(exclusion_path[0:-1])
+                else:
+                    pattern = '{}/*'.format(exclusion_path)
+                if re.match(pattern, path):
+                    return False
+        return True
+
+    def authorization_header(self, request=None) -> str:
+        """Function forauthorization header field from the request.
+        """
+        if request is not None:
+            return request.headers.get('Authorization', None)
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """Gets user from request.
+        """
+        return None
