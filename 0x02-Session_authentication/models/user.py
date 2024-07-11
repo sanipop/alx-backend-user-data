d1b50be6ef0547d14afe778684b469261c5229ec
sanipop <67932273+sanipop@users.noreply.github.com>
2024-07-11 01:11:22 +0100

    Create user.py

---
+++ 0x02-Session_authentication/models/user.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+""" Script for User module
+"""
+import hashlib
+from models.base import Base
+
+
+class User(Base):
+    """ Class for user
+    """
+
+    def __init__(self, *args: list, **kwargs: dict):
+        """ Initializer for user
+        """
+        super().__init__(*args, **kwargs)
+        self.email = kwargs.get('email')
+        self._password = kwargs.get('_password')
+        self.first_name = kwargs.get('first_name')
+        self.last_name = kwargs.get('last_name')
+
+    @property
+    def password(self) -> str:
+        """ Get password method
+        """
+        return self._password
+
+    @password.setter
+    def password(self, pwd: str):
+        """ Set new password: encrypt in SHA256
+        """
+        if pwd is None or type(pwd) is not str:
+            self._password = None
+        else:
+            self._password = hashlib.sha256(pwd.encode()).hexdigest().lower()
+
+    def is_valid_password(self, pwd: str) -> bool:
+        """ Validator for password
+        """
+        if pwd is None or type(pwd) is not str:
+            return False
+        if self.password is None:
+            return False
+        pwd_e = pwd.encode()
+        return hashlib.sha256(pwd_e).hexdigest().lower() == self.password
+
+    def display_name(self) -> str:
+        """ Display User name based on email/first_name/last_name
+        """
+        if self.email is None and self.first_name is None \
+                and self.last_name is None:
+            return ""
+        if self.first_name is None and self.last_name is None:
+            return "{}".format(self.email)
+        if self.last_name is None:
+            return "{}".format(self.first_name)
+        if self.first_name is None:
+            return "{}".format(self.last_name)
+        else:
+            return "{} {}".format(self.first_name, self.last_name)
